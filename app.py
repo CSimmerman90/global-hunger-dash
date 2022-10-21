9df05e57f2882e11148992ce09bd83f0184bf9ec
Chris Simmerman <chrisjsimmerman@gmail.com>
2022-10-20 20:44:56 -0400

    added second graph

--- app.py
+++ app.py
@@ -10,17 +10,35 @@ available_countries = df['entity'].unique()
 
 fig = px.line(df, x="year", y="global_hunger_index", color='entity', markers=True)
 
-app.layout = html.Div([
-        html.Br(),
-        html.Label('Multi-Select Dropdown'),
+app.layout = html.Div(children=[
+    html.Div([
+        html.H1(children='Global Hunger Index'),
+        html.Div(children='''
+            Analysis of the Global Hunger Index.
+        '''),
+        html.Label('Select Countries'),
         dcc.Dropdown(id= 'demo-dropdown', options=[{"value": i, "label": i} for i in available_countries],
                      value= ['Afghanistan'], multi=True),
         dcc.Graph(
-        id='display-selected-values',
-        figure=fig
-    ),
-])
+            id='display-selected-values',
+            figure=fig
+        ),
+    ]),
+    html.Div([
+        html.H1(children='Hello Dash V2'),
 
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+        html.Label('Multi-Select Dropdown 2'),
+        dcc.Dropdown(id= 'demo2-dropdown', options=[{"value": i, "label": i} for i in available_countries],
+                     value= ['Afghanistan'], multi=True),
+        dcc.Graph(
+            id='graph2',
+            figure=fig
+        ), 
+    ]),
+])
 
 @app.callback(
     dash.dependencies.Output('display-selected-values', 'figure'),
